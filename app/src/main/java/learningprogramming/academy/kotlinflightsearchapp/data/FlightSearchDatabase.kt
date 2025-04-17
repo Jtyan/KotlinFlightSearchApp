290e5164250cbe53f82266c59e4ba17079a9b327
Joeltt <joelttm15@gmail.com>
2025-04-17 15:25:12 +0100

    feat(db): implement FlightSearchDatabase using Room with existing asset

---
+++ app/src/main/java/learningprogramming/academy/kotlinflightsearchapp/data/FlightSearchDatabase.kt
@@ -0,0 +1,30 @@
+package learningprogramming.academy.kotlinflightsearchapp.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Airport::class, FavouriteAirport::class], version = 1, exportSchema = false)
+abstract class FlightSearchDatabase : RoomDatabase() {
+    abstract fun airportDAO(): AirportDAO
+    abstract fun favouriteAirportDAO(): FavouriteAirportDAO
+
+    companion object {
+        @Volatile
+        private var instance: FlightSearchDatabase? = null
+
+        fun getDatabase(context: Context): FlightSearchDatabase {
+            return instance ?: synchronized(this){
+                Room.databaseBuilder(
+                    context = context.applicationContext,
+                    FlightSearchDatabase::class.java,
+                    "flight_search_database")
+                    .createFromAsset("database/flight_search.db")
+                    .fallbackToDestructiveMigration(false)
+                    .build()
+                    .also { instance = it }
+            }
+        }
+    }
+}
