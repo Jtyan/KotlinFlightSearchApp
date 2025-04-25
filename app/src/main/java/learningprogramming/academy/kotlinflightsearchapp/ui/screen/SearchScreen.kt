1c014d61a45b02a88af9f9dbf5d747b8ca734b2a
Joeltt <joelttm15@gmail.com>
2025-04-25 14:13:28 +0100

    chore: change composable's name

--- app/src/main/java/learningprogramming/academy/kotlinflightsearchapp/ui/screen/SearchScreen.kt
+++ app/src/main/java/learningprogramming/academy/kotlinflightsearchapp/ui/screen/SearchScreen.kt
@@ -2,10 +2,8 @@ package learningprogramming.academy.kotlinflightsearchapp.ui.screen
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
@@ -26,10 +24,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import learningprogramming.academy.kotlinflightsearchapp.data.Airport
+import learningprogramming.academy.kotlinflightsearchapp.ui.components.FlightCodeAndName
 import learningprogramming.academy.kotlinflightsearchapp.viewmodel.FlightSearchViewModel
 
 @Composable
@@ -48,7 +44,7 @@ fun SearchScreen(
             query = searchQuery,
             onQueryChange = { viewModel.updateSearchQuery(it) },
             onSearch = {
-                if (searchQuery.isEmpty()) {
+                if(searchQuery.isEmpty()) {
                     viewModel.clearSelectedAirport()
                     onBackClick()
                 }
@@ -126,7 +122,7 @@ fun FlightSearchBar(
                 items(autoCompleteSuggestions) {
                     ListItem(
                         headlineContent = {
-                            FlightDetailCardAirport(
+                            FlightCodeAndName(
                                 code = it.iataCode,
                                 name = it.airportName
                             )
