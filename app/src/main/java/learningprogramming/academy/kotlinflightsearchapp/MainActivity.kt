1daac293c49b954cfc3032d433c5309b7435f65f
Joeltt <joelttm15@gmail.com>
2025-04-25 14:14:46 +0100

    feat: set up MainActivity to be FlightSearchAppNavigation

--- app/src/main/java/learningprogramming/academy/kotlinflightsearchapp/MainActivity.kt
+++ app/src/main/java/learningprogramming/academy/kotlinflightsearchapp/MainActivity.kt
@@ -4,13 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import learningprogramming.academy.kotlinflightsearchapp.ui.screen.FlightSearchAppNavigation
 import learningprogramming.academy.kotlinflightsearchapp.ui.theme.KotlinFlightSearchAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +13,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             KotlinFlightSearchAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                FlightSearchAppNavigation()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    KotlinFlightSearchAppTheme {
-        Greeting("Android")
-    }
-}
