65377cf682dc93105ae87a0f8c17a12686b062f0
Joeltt <joelttm15@gmail.com>
2025-04-25 13:48:21 +0100

    refactor: separate out component composables into component folder

--- app/src/main/java/learningprogramming/academy/kotlinflightsearchapp/ui/screen/HomeScreen.kt
+++ app/src/main/java/learningprogramming/academy/kotlinflightsearchapp/ui/screen/HomeScreen.kt
@@ -1,27 +1,15 @@
 package learningprogramming.academy.kotlinflightsearchapp.ui.screen
 
+import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.material.icons.filled.Star
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardColors
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SearchBar
@@ -30,26 +18,14 @@ import androidx.compose.material3.SearchBarDefaults.InputField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarColors
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.lifecycle.viewmodel.compose.viewModel
-import learningprogramming.academy.kotlinflightsearchapp.data.Airport
-import learningprogramming.academy.kotlinflightsearchapp.data.AirportRepository
-import learningprogramming.academy.kotlinflightsearchapp.data.AirportRepositoryImpl
+import learningprogramming.academy.kotlinflightsearchapp.ui.components.SelectedSearchList
+import learningprogramming.academy.kotlinflightsearchapp.ui.components.FavouritesList
 import learningprogramming.academy.kotlinflightsearchapp.viewmodel.FlightSearchViewModel
 
 
@@ -60,17 +36,13 @@ fun HomeScreen(
     onClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
-
+    val selectedAirport = viewModel.selectedAirport.collectAsState().value
+    val possibleDestinationsList by viewModel.possibleDestinations.collectAsState()
     val favouriteAirportList by viewModel.favouriteAirportList.collectAsState()
-    val possibleDestinations by viewModel.possibleDestinations.collectAsState()
-    val selectedAirport by viewModel.selectedAirport.collectAsState()
 
     Scaffold(
         topBar = {
-            FlightSearchTopAppBar(
-                scrollBehavior = scrollBehavior
-            )
+            FlightSearchTopAppBar()
         },
     ) { paddingValues ->
         Column(
@@ -84,35 +56,20 @@ fun HomeScreen(
                 onClick = onClick,
                 modifier = Modifier.align(Alignment.CenterHorizontally)
             )
-            if(selectedAirport == null) {
-                val favourites by viewModel.getAllFavouriteAirports().collectAsState()
-                FlightsLazyColumn(
-                    title = "Favourite routes",
-                    selectedAirport = null,
-                    items = favourites,
-                    renderItem = {favourite ->
-                        Text(
-                            text = "${favourite.departureCode} → ${favourite.destinationCode}",
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(16.dp)
-                        )
-                    },
-                    modifier = modifier
+            if (selectedAirport != null) {
+                Log.d("Column 1", "selectedAirport = $selectedAirport")
+                SelectedSearchList(
+                    viewModel = viewModel,
+                    title = "Flights from ${selectedAirport.iataCode}",
+                    favouriteAirportsList = favouriteAirportList,
+                    selectedAirport = selectedAirport,
+                    airportsList = possibleDestinationsList
                 )
             } else {
-                FlightsLazyColumn(
-                    title = "Flights from ${selectedAirport?.iataCode}",
-                    selectedAirport = selectedAirport,
-                    items = possibleDestinations,
-                    renderItem = { item ->
-                        FlightDetailsCard(
-                            viewModel = viewModel,
-                            selectedAirport = selectedAirport,
-                            possibleDestination = item
-                        )
-                    },
-                    modifier = modifier,
+                FavouritesList(
+                    viewModel = viewModel,
+                    title = "Favourite routes",
+                    favouriteAirportsList = favouriteAirportList
                 )
             }
         }
@@ -155,132 +112,9 @@ fun SearchBarButton(
     ) {}
 }
 
-@Composable
-fun <T> FlightsLazyColumn(
-    title: String,
-    selectedAirport: Airport?,
-    items: List<T>,
-    renderItem: @Composable (T) -> Unit,
-    modifier: Modifier = Modifier
-) {
-    Column(
-        modifier = modifier
-    ) {
-        Text(
-            text = title,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.padding(top = 12.dp)
-        )
-        LazyColumn {
-            items(items) {item ->
-                renderItem(item)
-            }
-        }
-    }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun FlightDetailsCard(
-    viewModel: FlightSearchViewModel,
-    selectedAirport: Airport?,
-    possibleDestination: Airport,
-    modifier: Modifier = Modifier
-) {
-    var isFavourite by remember { mutableStateOf(false) }
-    Card(
-        shape = RoundedCornerShape(
-            topStart = 0.dp,
-            topEnd = 12.dp,
-            bottomStart = 0.dp,
-            bottomEnd = 0.dp
-        ),
-        colors = CardColors(
-            contentColor = Color.Black,
-            containerColor = MaterialTheme.colorScheme.primaryContainer,
-            disabledContainerColor = MaterialTheme.colorScheme.surfaceContainerLow,
-            disabledContentColor = Color.Black
-        ),
-        modifier = Modifier.wrapContentSize().padding(vertical = 4.dp)
-
-    ) {
-        Row {
-            Column(
-                modifier = Modifier
-                    .weight(2f)
-                    .padding(4.dp)
-            ) {
-                Text(
-                    text = "DEPART",
-                    color = Color.DarkGray,
-                    fontSize = 12.sp,
-                    modifier = Modifier.padding(4.dp)
-                )
-                if (selectedAirport != null) {
-                    FlightDetailCardAirport(
-                        code = selectedAirport.iataCode,
-                        name = selectedAirport.airportName
-                    )
-                }
-                Text(
-                    text = "ARRIVE",
-                    color = Color.DarkGray,
-                    fontSize = 12.sp,
-                    modifier = Modifier.padding(4.dp)
-                )
-                FlightDetailCardAirport(
-                    code = possibleDestination.iataCode,
-                    name = possibleDestination.airportName
-                )
-            }
-            IconButton(
-                onClick = {},
-                modifier = Modifier.align(Alignment.CenterVertically)
-            ) {
-                Icon(
-                    imageVector = Icons.Default.Star,
-                    tint = if (isFavourite) Color.Red else Color.DarkGray,
-                    contentDescription = "favourite",
-                    modifier = Modifier
-                        .weight(1f)
-                        .size(28.dp)
-                        .align(Alignment.CenterVertically)
-                )
-            }
-        }
-    }
-}
-
-@Composable
-fun FlightDetailCardAirport(
-    code: String,
-    name: String,
-    modifier: Modifier = Modifier
-) {
-    Row(
-        modifier = modifier
-            .wrapContentSize()
-            .padding(4.dp)
-    ) {
-        Text(
-            text = code,
-            fontWeight = FontWeight.ExtraBold
-        )
-        Spacer(modifier = Modifier.width(4.dp))
-        Text(
-            text = name,
-            fontSize = 14.sp,
-            overflow = TextOverflow.Clip,
-            maxLines = 1,
-            color = Color.DarkGray
-        )
-    }
-}
-
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FlightSearchTopAppBar(
-    scrollBehavior: TopAppBarScrollBehavior,
     modifier: Modifier = Modifier
 ) {
     TopAppBar(
@@ -289,7 +123,6 @@ fun FlightSearchTopAppBar(
                 text = "Flight Search"
             )
         },
-        scrollBehavior = scrollBehavior,
         colors = TopAppBarColors(
             containerColor = MaterialTheme.colorScheme.onPrimaryContainer,
             scrolledContainerColor = MaterialTheme.colorScheme.onPrimaryContainer,
@@ -300,30 +133,3 @@ fun FlightSearchTopAppBar(
         modifier = modifier
     )
 }
-
-//@Preview(showSystemUi = true)
-//@Composable
-//fun HomeScreenPreview() {
-//    Scaffold(
-//        modifier = Modifier.statusBarsPadding()
-//    ) {
-//        Surface(
-//            modifier = Modifier
-//                .padding(it)
-//        ) {
-//            HomeScreen(
-//                viewModel = fakeViewModel,
-//                onClick = {}
-//            )
-//        }
-//    }
-//}
-
-//@Preview(showBackground = true)
-//@Composable
-//fun FlightDetailsCardPreview() {
-//    FlightDetailsCard(
-//        selectedAirport = Airport(1,"OPO", "Francisco Sá Carneiro Airport", 5053134),
-//        possibleDestination = Airport(7,"SVO", "Sheremetyevo - A.S. Pushkin international airport", 49933000)
-//    )
-//}
